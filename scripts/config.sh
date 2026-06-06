537ed375f2c538ed0e171bb6a8f0259d93d1ef61
Ronan-EruiniBennett <rheb99@icloud.com>
2026-06-06 18:12:21 +1000

    Fixed script config syntax

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,6 @@
 
-$TAG=infra-lab
-$HOST_PORT=5000
-$NAME=infra-lab-container
-$CONTAINER_PORT=5000
-$VM_IP=127.0.0.1
+TAG=infra-lab
+HOST_PORT=5000
+NAME=infra-lab-container
+CONTAINER_PORT=5000
+VM_IP=127.0.0.1
